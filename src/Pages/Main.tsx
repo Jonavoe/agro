5dc443b3e4be616f3e125831c2de101375f2b985
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-04-14 20:26:16 -0300

    Revert "Update Main.tsx"
    This reverts commit c73fba7e6a749ac94020ac89797495b92b674900.

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import ImageChange from '@/components/ImageChange'
+import Otro from '@/components/Otro'
+import Logo from '@/components/Logo'
 
 type Props = {}
 
 export default function Main({ }: Props) {
     return (
         <div className='flex flex-col font-cinzel animate-main'>
-            {/* <ImageChange />
+            <ImageChange />
             <Logo />
-            <Otro /> */}
+            <Otro />
         </div>
     )
 }
