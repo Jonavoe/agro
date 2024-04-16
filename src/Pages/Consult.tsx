954f8d8e3d1395a35a6f827c0fade648cbe56431
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-04-16 01:24:58 -0300

    Create Consult.tsx

---
+++ src/Pages/Consult.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Consult({ }: Props) {
+    return (
+        <div className='z-10 bg-transparent w-full h-64 flex justify-center items-center text-[--white] drop-shadow-lg'>
+            <div className='lg:w-1/2 w-3/4 text-center'>
+                <h3 className='uppercase text-5xl'>CONSULTAS</h3>
+                <p className='text-2xl'>Comience un vínculo comercial eficiente y rentable.</p>
+                <p className='text-5xl'>(3487) 229328</p>
+            </div>
+        </div>
+    )
+}
