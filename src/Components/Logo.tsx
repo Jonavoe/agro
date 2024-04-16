9b91234e2d0385bd415a8a421b97aaf68ce196a4
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-04-15 22:37:44 -0300

    Update Logo.tsx

--- src/Components/Logo.tsx
+++ src/Components/Logo.tsx
@@ -6,20 +6,20 @@ type Props = {}
 
 export default function Logo({ }: Props) {
     return (
-        <div className='flex flex-col gap-2 z-10 h-screen items-center mt-36 '>
-            <div className='w-72'>
+        <div className='flex flex-col gap-2 z-10 h-screen items-center pt-36 '>
+            <div className='w-72 rounded-lg'>
 
-                <Image src={LogoImage} alt='Logo' className=' animate-image' />
+                <Image src={LogoImage} alt='Logo' className='animate-image rounded-lg' />
             </div>
             <div className='w-auto h-auto flex flex-col items-center justify-center'>
-                <span className='w-full h-1 bg-[--white] animate-span'></span>
-                <h1 className='px-2 uppercase text-5xl font-semibold text-[--principal]'>Belloni</h1>
-                <span className='w-full h-1 bg-[--white] animate-span'></span>
+                <span className='w-full h-1 bg-[--white] animate-span rounded-sm'></span>
+                <h1 className='px-9 uppercase text-5xl font-semibold text-[--principal]'>Belloni</h1>
+                <span className='w-full h-1 bg-[--white] animate-span rounded-sm'></span>
             </div>
             <p className='uppercase text-[--white] font-semibold drop-shadow-lg'>Profesionales del agro</p>
 
-            <div className='bg-[--white] hover:bg-[--hover-white] rounded-md px-6 py-2'>
-                <p className='uppercase text-[--principal] font-semibold drop-shadow-lg'>
+            <div className='bg-[--white] text-[--principal] hover:text-[--hover-principal] hover:bg-[--hover-white] rounded-md px-6 py-2 transition-all ease-in-out duration-500'>
+                <p className='uppercase  font-semibold drop-shadow-lg'>
                     Bienvenido
                 </p>
             </div>
