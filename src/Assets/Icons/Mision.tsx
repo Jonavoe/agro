75672d5939432d66a10916b3f246b060d620ae73
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-04-15 22:37:35 -0300

    Create Mision.tsx

---
+++ src/Assets/Icons/Mision.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Mision: React.FC = () => {
+    return (
+        <svg
+            height="50px"
+            width="50px"
+            version="1.1"
+            id="Layer_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            viewBox="0 0 512 512"
+            xmlSpace="preserve"
+            fill="#ffffff"
+            stroke="#ffffff"
+        >
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+            <g id="SVGRepo_iconCarrier">
+                <g>
+                    <path style={{ fill: '#ffffff' }} d="M105.408,150.296L0,202.872v277.68h105.408V153.864V150.296z"></path>
+                    <path
+                        style={{ fill: '#ffffff' }}
+                        d="M196.464,104.872L136.24,134.92v345.632h105.408V137.544L196.464,104.872L196.464,104.872z"
+                    ></path>
+                    <path
+                        style={{ fill: '#ffffff' }}
+                        d="M271.76,159.496v321.072h105.408V170.2L332,202.872L271.76,159.496L271.76,159.496z"
+                    ></path>
+                    <path style={{ fill: '#ffffff' }} d="M512,72.68l-105.408,76.224v331.648H512V72.2V72.68z"></path>
+                </g>
+                <polygon points="430.72,37.496 489.376,31.432 464.544,84.824 "></polygon>
+                <polygon points="332.256,155.24 195.952,56.696 7.152,151.304 0,136.984 197.488,38.04 332.256,135.496 462.768,40.312 472.144,53.288 "></polygon>
+            </g>
+        </svg>
+    );
+};
+
+export default Mision;
