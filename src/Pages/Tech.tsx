5baf9f97931cbf7e8b427648056b86c128bce9db
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-04-15 22:38:03 -0300

    Create Tech.tsx

---
+++ src/Pages/Tech.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Tech({ }: Props) {
+    return (
+        <div className='z-10 bg-transparent w-full h-64 flex justify-center items-center text-[--white] drop-shadow-lg'>
+            <div className='lg:w-1/2 w-3/4 text-center'>
+                <p className='text-2xl lg:text-4xl'>Tecnología para mejorar sus resultados. Compromiso para cuidar su capital. Profesionalismo para aumentar su rentabilidad.</p>
+            </div>
+        </div>
+    )
+}
