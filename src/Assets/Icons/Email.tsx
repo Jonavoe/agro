9007f512b9485d25169934f40361255be2b598c9
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-04-16 09:42:11 -0300

    Update Email.tsx

--- src/Assets/Icons/Email.tsx
+++ src/Assets/Icons/Email.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-const Email: React.FC = () => {
+interface EmailProps {
+    color: string;
+}
+
+const Email: React.FC<EmailProps> = ({ color }) => {
     return (
         <svg
             viewBox="0 -3.5 32 32"
@@ -8,7 +12,7 @@ const Email: React.FC = () => {
             version="1.1"
             xmlns="http://www.w3.org/2000/svg"
             xmlnsXlink="http://www.w3.org/1999/xlink"
-            fill="#367c2b"
+            fill={color}
         >
             <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
             <g
@@ -30,7 +34,7 @@ const Email: React.FC = () => {
                     <g
                         id="Icon-Set-Filled"
                         transform="translate(-414.000000, -261.000000)"
-                        fill="#367c2b"
+                        fill={color}
                     >
                         <path
                             d="M430,275.916 L426.684,273.167 L415.115,285.01 L444.591,285.01 L433.235,273.147 L430,275.916 L430,275.916 Z M434.89,271.89 L445.892,283.329 C445.955,283.107 446,282.877 446,282.634 L446,262.862 L434.89,271.89 L434.89,271.89 Z M414,262.816 L414,282.634 C414,282.877 414.045,283.107 414.108,283.329 L425.147,271.927 L414,262.816 L414,262.816 Z M445,261 L415,261 L430,273.019 L445,261 L445,261 Z"
