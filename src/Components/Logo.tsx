186ed203f10d0ea237c6917026028628603e8ff3
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-04-16 09:52:00 -0300

    Update Logo.tsx

--- src/Components/Logo.tsx
+++ src/Components/Logo.tsx
@@ -1,13 +1,15 @@
 import Image from 'next/image'
 import React from 'react'
 import LogoImage from "@/Assets/Img/Logo.png"
+import { Link as ScrollLink, animateScroll as scroll } from 'react-scroll';
+
 
 type Props = {}
 
 export default function Logo({ }: Props) {
     return (
-        <div className='flex flex-col gap-2 z-10 h-screen items-center pt-36 '>
-            <div className='w-72 rounded-lg'>
+        <div className='flex flex-col gap-2 z-10 h-screen items-center lg:pt-40 py-10'>
+            <div className='lg:w-72 w-36 rounded-lg'>
 
                 <Image src={LogoImage} alt='Logo' className='animate-image rounded-lg' />
             </div>
@@ -18,11 +20,16 @@ export default function Logo({ }: Props) {
             </div>
             <p className='uppercase text-[--white] font-semibold drop-shadow-lg'>Profesionales del agro</p>
 
-            <div className='bg-[--white] text-[--principal] hover:text-[--hover-principal] hover:bg-[--hover-white] rounded-md px-6 py-2 transition-all ease-in-out duration-500'>
+            <ScrollLink
+                to="company"
+                smooth={true}
+                duration={500}
+                className='bg-[--white] text-[--principal] hover:text-[--hover-principal] hover:bg-[--hover-white] rounded-md px-6 py-2 transition-all ease-in-out duration-500'
+            >
                 <p className='uppercase  font-semibold drop-shadow-lg'>
                     Bienvenido
                 </p>
-            </div>
+            </ScrollLink>
         </div>
     )
 }
